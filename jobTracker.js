07173547a08347d79af2bcb14df0cfa50c114720
Avery Broderick <ave.broderick@gmail.com>
2017-05-30 10:06:39 -0400

    empty model added to model branch

---
+++ jobTracker.js
@@ -0,0 +1,21 @@
+//Model
+
+var model = {
+  jobs: {},
+  currentKey: 0
+};
+
+// var sample = {
+//   position: 'Web Dev',
+//   company: 'Google',
+//   location: 'Indeed',
+//   applied: false
+// }
+
+// var sample2 = {
+//   position: 'Secretary',
+//   company: 'Tiffany & Co.',
+//   location: 'Monster',
+//   applied: false
+// }
+
